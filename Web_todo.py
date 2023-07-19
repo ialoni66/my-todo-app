1c5fc1c2bfd700f347419d81418385d241e3ff70
ilai <ilai.aloni@gmail.com>
2023-07-19 14:13:02 -0700

    first web app uploaded and updated

--- Web_todo.py
+++ Web_todo.py
@@ -12,7 +12,11 @@ def add_todo():
 
 st.title("My Todo App")
 st.subheader("This is my todo App!")
-st.write("This app will increase your productivity.")
+st.write("This app will increase your <b>productivity</b>.",
+         unsafe_allow_html=True)
+
+st.text_input(label="", placeholder="Add a new to-do",
+              on_change=add_todo, key='new_todo')
 
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=todo)
@@ -22,5 +26,4 @@ for index, todo in enumerate(todos):
         del st.session_state[todo]
         st.experimental_rerun()
 
-st.text_input(label="", placeholder="Add a new to-do",
-              on_change=add_todo, key='new_todo')
+
